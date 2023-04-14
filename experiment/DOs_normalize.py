b4968c0c4934e297b4daeb56d3aede3b42ab9abc
Beihai <2267601646@qq.com>
2023-04-14 16:01:56 +0800

    补充了扩展矩阵，下一步是要对拓展的矩阵进行加密

--- experiment/DOs_normalize.py
+++ experiment/DOs_normalize.py
@@ -23,19 +23,6 @@ def normalize_global():
     # print(f'before glo_norm{X}')
     return X
 
-def upload_data():
-    """
-    将本地数据加密，上传
-    :return: 密文数据，对应的明文是归一化之后的
-    """
-    pk = public_key()
-    X_gnorm = normalize_global()
-    for X_i in X_gnorm:
-        for i in range(len(X_i)):
-            en_X = TA.encrypt(pk, X_i.iloc[i]) #返回的是字典
-            X_i.iloc[i] = en_X['ct_list']
-    return X_gnorm
-
 def extend_sample(sample):
     '''
     DOi extend each sample data into a matrix
@@ -56,9 +43,51 @@ def extend_sample(sample):
     M = M.T
     return M
 
-# 调试用
-# x =  [random.uniform(-0.05, 1.05) for _ in range(9)]
-# M = extend_sample(x)
+def extend_to_matrix():
+    """
+    获得归一化后的数据，并将其扩展为一个矩阵
+    :return: M
+    """
+    X = normalize_global()
+    Mi = []
+    for xi in X:
+        for row in xi.iterrows():
+            sample = list(row[1]) # iterrows()的用法，row[0]是行号，row[1]是对应数据
+            Mik = extend_sample(sample)
+            Mi.append(Mik)
+    # 当第二个循环后，Mi不断拓展数据，最后变为M
+    return Mi
 
-# X = upload_data()
+def encrypt_data():
+    """
+    将本地数据加密
+    :return: 密文数据，对应的明文是归一化之后的
+    """
+    pk = public_key()
+    M = extend_sample()
+    X_gnorm = 1+1
+    for X_i in X_gnorm:
+        for i in range(len(X_i)):
+            en_X = TA.encrypt(pk, X_i.iloc[i]) # 返回的是字典
+            X_i.iloc[i] = en_X['ct_list']
+    return X_gnorm
 
+def upload_data():
+    """
+    将本地数据加密，上传
+    :return: 密文数据，对应的明文是归一化之后的
+    """
+    pk = public_key()
+    X_gnorm = normalize_global()
+    for X_i in X_gnorm:
+        for i in range(len(X_i)):
+            en_X = TA.encrypt(pk, X_i.iloc[i]) # 返回的是字典
+            X_i.iloc[i] = en_X['ct_list']
+    return X_gnorm
+
+def str_to_int(numbers):
+    numbers = list(map(int, numbers))  # 把字符串型转为数值型，注意这里转的是整数
+    return numbers
+
+X = normalize_global()
+print(X)
